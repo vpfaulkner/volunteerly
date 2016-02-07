a1891e8dae29e65d00e9482dac71f280a598ac8a
Vance Faulkner <vance@validic.com>
2016-02-06 19:59:24 -0500

    Added admin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root 'assignments#index'
 
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
+
+  resources :assignments
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
